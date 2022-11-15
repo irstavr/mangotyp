80ccb15003755452f33c26ed85c2326d31365a5c
irstavr <irini.stavrakantonaki@koahealth.com>
2022-11-15 16:06:38 +0100

    read the contents of our Rust file read input Rust file and crete a syn::File with it
    we read the file's contents by using the std lib, File, Read and Path
    then parse it into a syn::File where we can iterate over the contents of the file in a typesafe manner.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use clap::{Arg, Command};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 fn main() {
      let matches = Command::new("magotyp")
@@ -21,11 +26,31 @@ fn main() {
         )
         .get_matches();
 
-    let input_filename = matches.get_one::<String>("input").expect("input required");
+    let input_filename = matches
+        .get_one::<String>("input")
+        .expect("input required");
+        
     let output_filename = matches
         .get_one::<String>("output")
         .expect("output required");
 
     dbg!(input_filename);
     dbg!(output_filename);
+
+    let input_path = Path::new(input_filename);
+
+    let mut input_file =
+        File::open(input_path).
+            expect(&format!("Cannot open file {}", input_path.display()));
+
+    let mut input_file_text = String::new();
+
+    input_file
+        .read_to_string(&mut input_file_text)
+        .expect("Unable to read file");
+
+    // This is our tokenized version of Rust file ready to process
+    let input_syntax: syn::File = 
+        syn::parse_file(&input_file_text).expect("Cannot parse file!");
+
 }
