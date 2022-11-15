4c59a1ed6986dc517cb93bc3e7b68def6acfeb3d
irstavr <irini.stavrakantonaki@koahealth.com>
2022-11-15 15:56:18 +0100

    use clap to enforce required args by user
    It forces the user to define both a input filename (Rust) and an output filename (Typescript).
    Also it helps to know the available commands via Rust CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+     let matches = Command::new("magotyp")
+        .version("1.0.0")
+        .author("Irini St")
+        .about("From Rust types to Typescript types")
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .required(true)
+                .help("The input Rust file"),
+        )
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .required(true)
+                .help("The output Typescript file"),
+        )
+        .get_matches();
+
+    let input_filename = matches.get_one::<String>("input").expect("input required");
+    let output_filename = matches
+        .get_one::<String>("output")
+        .expect("output required");
+
+    dbg!(input_filename);
+    dbg!(output_filename);
 }
